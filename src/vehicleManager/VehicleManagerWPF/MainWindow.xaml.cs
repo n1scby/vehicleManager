6c246609b3434c23c320d02f192f1d83d0b94035
Nic B <n1scby@yahoo.com>
2018-01-16 15:08:21 -0500

    wpf app

---
+++ src/vehicleManager/VehicleManagerWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VehicleLibrary;
+
+namespace VehicleManagerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Vehicle auto = new Vehicle();
+            auto.Year = int.Parse(YearTextBox.Text);
+            auto.Make = MakeTextBox.Text;
+            auto.Model = ModelTextBox.Text;
+
+            OutputTextBlock.Text = auto.getVehicle();
+        }
+    }
+}
